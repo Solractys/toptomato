94bda9d5d677715c5fd9425a20eb32d467c10f17
Solractys <carloseduardosilvasouzadelima@gmail.com>
2025-05-08 01:41:22 -0300

    head

---
+++ src/app/head.tsx
@@ -0,0 +1,16 @@
+export default function Head() {
+  return (
+    <>
+      <title>TopTomato - Pomodoro Timer</title>
+      <meta
+        name="description"
+        content="A simple Pomodoro timer to boost your productivity"
+      />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9030427245078103"
+        crossOrigin="anonymous"
+      ></script>
+    </>
+  );
+}
